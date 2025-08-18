827a5f3dff8b6e87c2112134a672b0caffa3172e
Sandra <sandra.mstowskaa@gmail.com>
2025-08-18 11:05:49 +0200

    complete task 04- complete all tasks

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,22 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
-        month: '04',
+        day: '18',
+        month: '08',
         year: '1985'
     }
 }
+
+function happyBday(user) {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+
+    if (day == user.born.day && month == user.born.month) {
+        return "Today is your birthday!";
+    } else {
+    return "Today is not your birthday :c";
+    }
+}
+
+console.log(happyBday(user));
